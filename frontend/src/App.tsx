07ceab523aab923bb781ea554c47ea1921125ea3
Philip Zhang <code@philipzha.ng>
2023-12-20 18:33:23 -0800

    styled components theme provider and global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { Login, Home } from "@/pages";
+import { GlobalStyle, theme } from "@/theme";
+import { ThemeProvider } from "styled-components";
 
 export default function App() {
   return (
-    <HelmetProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </HelmetProvider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <HelmetProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </HelmetProvider>
+    </ThemeProvider>
   );
 }
---
+++ frontend/src/theme.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+// Store global theme values (e.g. colors) here.
+// This is object is accessible via the `theme` prop on all styled components.
+// See: https://styled-components.com/docs/advanced#theming
+export const theme = {
+  colors: {
+    // primary: "",
+    // background: "",
+    // ...etc
+  },
+};
+
+// Global CSS applied to every page.
+export const GlobalStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+`;
