76875935ff3f8890b927c5f45e35afa7963168a0
Shreya Gupta <94256244+shreyagupta112@users.noreply.github.com>
2024-01-21 14:04:20 -0800

    Correct skeleton code for backend
    Accidentally uploaded the wrong file last time, this is the correct one

--- backend/src/routes/units.ts
+++ backend/src/routes/units.ts
@@ -1,45 +1,18 @@
-import { APIResult, get, handleAPIError } from "./requests";
+import express from "express";
+const router = express.Router();
 
-export type Unit = {
-    approved: boolean;
-    landlordFirstName: string;
-    landlordLastName: string;
-    landlordEmail: string;
-    landlordPhone: string;
-    monthlyRent: number;
-    securityDeposit: number;
-    paymentRentingCriteria: string[];
-    applicationFeeCost?: number;
-    housingAuthority: string;
-    holdingFeeAmount?: number;
-    listingAddress: string;
-    sqft: number;
-    dateAvailable: Date;
-    availableNow: boolean;
-    numBeds: number;
-    numBaths: number;
-    appliances: string[];
-    communityFeatures: string[];
-    parking: string[];
-    accessibility: string[];
-    pets: string[];
-    sharingAcceptable: string;
-    landlordComments: string;
-    subsidyType?: string;
-    whereFound?: string;
-    additionalRules?: string[];
-    internalNotes?: string;
-    createdAt: Date;
-    updatedAt: Date;
-  };
-  
+router.get('/units/:id', (req, res) => {
 
-export async function getUnit(id: string): Promise<APIResult<Unit>> {
-    try {
-      const response = await get("/units/${id}");
-      const json = (await response.json()) as Unit;
-      return { success: true, data: json };
-    } catch (error) {
-      return handleAPIError(error);
+});
+
+/*
+const Unit = require("../models/UnitModel");
+router.get('/units/:id', (req, res) => {
+    Unit.findById(req.params.id)
+    .then((unit) => res.json(unit))
+    .catch(errorHandler(res))
     }
-  }
+  );
+*/
+
+export default router;
