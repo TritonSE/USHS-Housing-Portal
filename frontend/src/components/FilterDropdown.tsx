79ba71a20a076db99e3c84e12f430b454a01b1b0
nbpillai <nipillai@ucsd.edu>
2024-01-21 10:52:29 -0800

    first commit

---
+++ frontend/src/components/FilterDropdown.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+// export type FilterDropdownProps = {
+//     children: React.ReactNode;
+//   };
+
+//   export const FilterDropdown = ({ children }: FilterDropdownProps) => {
+//     return (
+
+//     );
+//   };
