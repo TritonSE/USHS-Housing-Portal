3d8547e995204e143a63cb6cea9524860d2fa59d
Shreya Gupta <94256244+shreyagupta112@users.noreply.github.com>
2024-01-26 17:41:46 -0800

    Add basic HTML and Styles to frontend
    Incomplete

--- frontend/src/pages/UnitDetails.tsx
+++ frontend/src/pages/UnitDetails.tsx
@@ -6,92 +6,145 @@ import { Unit, getUnit } from "@/api/units";
 import { Page } from "@/components";
 // import { privateDecrypt } from "crypto";
 
-export function UnitDetails() {
-  const [unit, setUnit] = useState<Unit>();
-  const { id } = useParams();
+import { render } from "react-dom";
+import styled from "styled-components";
 
-  React.useEffect(() => {
-    if (id !== undefined) {
-      void getUnit(id).then((result) => {
-        if (result.success) {
-          setUnit(result.data);
-        }
-      });
-    }
-  }, []);
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: #fbf7f3;
+  padding: 0 11%;
+`;
 
-  if (unit === undefined) {
-    return (
-      <Page>
-        <Helmet>
-          <title>Unit Does Not Exist | USHS Housing Portal</title>
-        </Helmet>
-        {/* add button */}
-        <h1>This unit does not exist!</h1>
-      </Page>
-    );
-  }
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #fbf7f3;
+  jusify-content: evenly-spaced;
+`;
 
-  return (
-    <Page>
-      <Helmet>
-        <title>{unit._id} | USHS Housing Portal</title>
-      </Helmet>
-      {/* add button */}
-      <h2>{unit.monthlyRent}/month</h2>
-      <h1>{unit.listingAddress}</h1>
-      <h2>Fees </h2>
-      <h3>Security Deposit: </h3>
-      <ul>
-        <li>{unit.securityDeposit}</li>
-      </ul>
-      <h3>Application Fee: </h3>
-      <ul>
-        <li>{unit.applicationFeeCost}</li>
-      </ul>
-      <h3>Payment/Renting Criteria: </h3>
-      <ul>
-        <li>{unit.paymentRentingCriteria}</li>
-      </ul>
-      <h3>Holding Fee; </h3>
-      <ul>
-        {/* check to see if the holding fee amount is needed and 
-            base ouput on that  */}
-        <li>{unit.holdingFeeAmount}</li>
-      </ul>
-      <h2> Housing Specifications: </h2>
-      <h3>Parking: </h3>
-      <ul>
-        <li>{unit.parking}</li>
-      </ul>
-      <h3>Accessibility Access:</h3>
-      <ul>
-        <li>{unit.accessibility}</li>
-      </ul>
-      <h3>Pets/Animals</h3>
-      <ul>
-        <li>{unit.pets}</li>
-      </ul>
-      <h3>Sharing House Acceptable:</h3>
-      <ul>
-        <li>{unit.sharingAcceptable}</li>
-      </ul>
-      <h3>Appliances: </h3>
-      <ul>
-        <li>{unit.appliances}</li>
-      </ul>
-      <h3>Community/Neighborhood Information: </h3>
-      <ul>
-        <li>{unit.communityFeatures}</li>
-      </ul>
-      <h3>Housing Authority: </h3>
-      <ul>
-        <li>{unit.housingAuthority}</li>
-      </ul>
-      <h3>Additional Comments from Landlord: </h3>
-      <ul>
-        <li>{unit.additionalRules}</li>
-      </ul>
-    </Page>
-  );
-}
+const DetailsRow = styled(Row)`
+  gap: 10px;
+`;
+
+const SectionRow = styled(Row)`
+  gap: 10px;
+`;
+
+const SubSectionRow = styled(Row)`
+  justify-content: space-evenly;
+`;
+
+const RentPerMonth = styled.h1`
+  font-size: 48px;
+  font-family: "Neutra Text";
+  font-weight: 700;
+  line-height: 150%;
+  line-spacing: 0.96px;
+
+  margin: 0;
+`;
+
+const Header = styled.div`
+  font-size: 32px;
+  margin: 0;
+  font-weight: 700;
+  font-family: "Neutra Text";
+  line-height: 150%;
+  line-spacing: 0.64px;
+`;
+
+const Text = styled.div`
+  font-family: "Montserrat"
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 150%
+  letter-spacing: 0.4px;
+`;
+
+const StrongText = styled(Text)`
+  font-weight: 600;
+`;
+
+const ListText = styled(Text)``;
+
+const Address = styled(Header)``;
+
+const UnitDetails = () => (
+  <Column>
+    <Row>
+      <RentPerMonth>123/month</RentPerMonth>
+    </Row>
+    <Row>
+      <Address>123 a st.</Address>
+    </Row>
+    <DetailsRow>
+      <Column>
+        <StrongText>2</StrongText>
+        <Text>beds</Text>
+      </Column>
+      <Column>
+        <StrongText>2</StrongText>
+        <Text>beds</Text>
+      </Column>
+      <Column>
+        <StrongText>2</StrongText>
+        <Text>beds</Text>
+      </Column>
+      <Column>
+        <StrongText>Available Text</StrongText>
+      </Column>
+    </DetailsRow>
+
+    <SectionRow>
+      <Header>Fees</Header>
+    </SectionRow>
+    <SubSectionRow>
+      <Column>
+        <StrongText>Security Deposit: </StrongText>
+        <ListText> $1390</ListText>
+        <StrongText>Payment/Renting Criteria: </StrongText>
+        <ListText> Credit check required</ListText>
+      </Column>
+      <Column>
+        <StrongText>Applicaton Fee: </StrongText>
+        <ListText> $20</ListText>
+        <StrongText>Holding Fee: </StrongText>
+        <ListText> Not Applicable</ListText>
+      </Column>
+    </SubSectionRow>
+
+    <SectionRow>
+      <Header>Housing Specifications</Header>
+    </SectionRow>
+    <SubSectionRow>
+      <Column>
+        <StrongText>Parking: </StrongText>
+        <StrongText>Pets/Animals: </StrongText>
+        <StrongText>Appliances: </StrongText>
+        <StrongText>Housing Authority: </StrongText>
+        <StrongText>Additional Comments from Landlord: </StrongText>
+      </Column>
+      <Column>
+        <StrongText>Accessibility Access: </StrongText>
+        <StrongText>Sharing House Acceptable: </StrongText>
+        <StrongText>Community/Neighborhood Information: </StrongText>
+      </Column>
+    </SubSectionRow>
+
+    <SectionRow>
+      <Header>Additional Information</Header>
+    </SectionRow>
+    <SubSectionRow>
+      <Column>
+        <StrongText>Where Was Unit Found: </StrongText>
+        <StrongText>Additional Rules and Regulation: </StrongText>
+      </Column>
+      <Column>
+        <StrongText>Notes from Housing Locator: </StrongText>
+      </Column>
+    </SubSectionRow>
+  </Column>
+);
+
+render(<UnitDetails />, document.getElementById("root"));
