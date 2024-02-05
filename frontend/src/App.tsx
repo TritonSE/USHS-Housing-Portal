4a60c6084ef69a3fe8e681133618d4ec68225b63
Azhou2023 <anz008@ucsd.edu>
2024-02-05 01:09:54 -0800

    commented out DataContext for pr

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 import { AuthContext, AuthProvider } from "./contexts/AuthContext";
-import { DataProvider } from "./contexts/DataContext";
+// import { DataProvider } from "./contexts/DataContext";
 
 import { Home, Login, Profile } from "@/pages";
 import { GlobalStyle, theme } from "@/theme";
@@ -37,12 +37,12 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <AuthProvider>
-        <DataProvider>
-          <GlobalStyle />
-          <HelmetProvider>
-            <AppRouter />
-          </HelmetProvider>
-        </DataProvider>
+        {/* <DataProvider> */}
+        <GlobalStyle />
+        <HelmetProvider>
+          <AppRouter />
+        </HelmetProvider>
+        {/* </DataProvider> */}
       </AuthProvider>
     </ThemeProvider>
   );
