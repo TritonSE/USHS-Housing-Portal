3dde81e91b1f04224d6abc56d8c3b17c57631c98
Philip Zhang <code@philipzha.ng>
2024-02-06 21:10:31 -0800

    remove auth context testing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,45 +1,21 @@
-import { signOut } from "firebase/auth";
-import { useContext } from "react";
+import React from "react";
 import { Helmet } from "react-helmet-async";
-import { useNavigate } from "react-router-dom";
 
+import { getUsers } from "@/api/users";
 import { Page } from "@/components";
-import { AuthContext } from "@/contexts/AuthContext";
-import { auth } from "@/firebase";
 
 export function Home() {
-  const authContext = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  //Everything below is testing for auth context, feel free to delete
-  const logOut = () => {
-    signOut(auth)
-      .then(() => {
-        console.log("success");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  React.useEffect(() => {
+    // Example API call
+    void getUsers().then(console.log);
+  }, []);
 
   return (
     <Page>
       <Helmet>
         <title>Home | USHS Housing Portal</title>
       </Helmet>
-      {authContext.signedIn ? (
-        <h1>{"Welcome, " + authContext.currentUser?.displayName}</h1>
-      ) : (
-        <h1>Please log in to begin.</h1>
-      )}
-      <button
-        onClick={() => {
-          navigate("/login");
-        }}
-      >
-        Login
-      </button>
-      <button onClick={logOut}>Log Out</button>
+      <h1>This is the home page.</h1>
     </Page>
   );
 }
