fe62d8832b328488ec2cf6d3539e80dd6b2ecf9b
Yashwanth Ravipati <63521936+YashRavipati1@users.noreply.github.com>
2024-02-06 22:12:24 -0800

    Feature/andrew yash/frontend contexts (#41)
    Resolves #15
    
    <!-- What changes did you make? -->
    
    Adding auth and data contexts to use instead of making backend/firebase
    calls every time we need user information.
    
    <!-- How did you confirm your changes worked? -->
    
    Tested the contexts on the Login page by printing out user information
    from the contexts.
    
    <!-- Upload a screenshot, if possible. Otherwise, please provide
    instructions on how to see the change. -->
    
    <img width="1240" alt="Screenshot 2024-02-05 at 6 17 58 PM"
    src="https://github.com/TritonSE/USHS-Housing-Portal/assets/63521936/faf35b7c-e307-46cf-869e-4c21763be765">
    
    ---------
    
    Co-authored-by: Azhou2023 <anz008@ucsd.edu>
    Co-authored-by: Philip Zhang <code@philipzha.ng>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,29 @@ import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
+import { AuthProvider } from "./contexts/AuthContext";
+import { DataProvider } from "./contexts/DataContext";
+
 import { Home, Login, UnitDetails } from "@/pages";
 import { GlobalStyle, theme } from "@/theme";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <HelmetProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/unit/:id" element={<UnitDetails />} />
-          </Routes>
-        </BrowserRouter>
-      </HelmetProvider>
+      <AuthProvider>
+        <DataProvider>
+          <GlobalStyle />
+          <HelmetProvider>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/unit/:id" element={<UnitDetails />} />
+              </Routes>
+            </BrowserRouter>
+          </HelmetProvider>
+        </DataProvider>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
