55ab8e980e54cb0f050c05ba0a649730999ed8a9
Philip Zhang <code@philipzha.ng>
2024-02-14 22:53:35 -0800

    frontend post api function

--- backend/src/routes/units.ts
+++ backend/src/routes/units.ts
@@ -1,5 +1,5 @@
 /**
- * User router
+ * Units router
  */
 
 import express from "express";
--- frontend/src/api/units.ts
+++ frontend/src/api/units.ts
@@ -1,5 +1,42 @@
 import { APIResult, handleAPIError, post } from "src/api/requests";
 
+// Represents a Unit object as it will be received from the backend.
+export type Unit = {
+  landlordFirstName: string;
+  landlordLastName: string;
+  landlordEmail: string;
+  landlordPhone: string;
+  streetAddress: string;
+  suiteNumber: string;
+  city: string;
+  state: string;
+  areaCode: string;
+  sqft: number;
+  monthlyRent: number;
+  securityDeposit: number;
+  acceptThirdParty: boolean;
+  housingAuthority: string;
+  applicationFeeCost: number;
+  dateAvailable: string;
+  availableNow: boolean;
+  numBeds: number;
+  numBaths: number;
+  appliances: string[];
+  communityFeatures: string[];
+  parking: string[];
+  accessibility: string[];
+  pets: string[];
+  sharingAcceptable: string;
+  landlordComments: string;
+  whereFound: string;
+  paymentRentingCriteria: string[];
+  additionalRules: string[];
+  internalComments: string;
+  approved: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
+
 export type FilterParams = {
   search?: string | undefined;
   availability: number;
@@ -11,59 +48,43 @@ export type FilterParams = {
   sort: number;
 };
 
-export function getUnits(params: FilterParams): Promise<APIResult<Unit[]>> {
-  try {
-    const query = new URLSearchParams(params);
+// export function getUnits(params: FilterParams): Promise<APIResult<Unit[]>> {
+//   try {
+//     const query = new URLSearchParams(params);
 
-    const keysForDel: string[] = [];
-    query.forEach((value, key) => {
-      if (value === "" || value === null || value === "undefined") {
-        keysForDel.push(key);
-      }
-    });
+//     const keysForDel: string[] = [];
+//     query.forEach((value, key) => {
+//       if (value === "" || value === null || value === "undefined") {
+//         keysForDel.push(key);
+//       }
+//     });
 
-    keysForDel.forEach((key) => {
-      query.delete(key);
-    });
+//     keysForDel.forEach((key) => {
+//       query.delete(key);
+//     });
 
-    console.log(query.toString());
-  } catch (error) {
-    return handleAPIError(error);
-  }
-}
+//     console.log(query.toString());
+//   } catch (error) {
+//     return handleAPIError(error);
+//   }
+// }
 
-export type CreateUnitRequest = {
-  approved: boolean;
-  landlordFirstName: string;
-  landlordLastName: string;
-  landlordEmail: string;
-  landlordPhone: string;
-  monthlyRent: number;
-  securityDeposit: number;
-  paymentRentingCriteria: [string];
-  applicationFeeCost: number;
-  housingAuthority: string;
-  holdingFeeAmount: number;
-  listingAddress: string;
-  sqft: number;
-  dateAvailable: Date;
-  availableNow: boolean;
-  numBeds: number;
-  numBaths: number;
-  appliances: [string];
-  communityFeatures: [string];
-  parking: [string];
-  accessibility: [string];
-  pets: [string];
-  sharingAcceptable: string;
-  landlordComments: string;
-};
+type HousingLocatorFields =
+  | "whereFound"
+  | "paymentRentingCriteria"
+  | "additionalRules"
+  | "internalComments"
+  | "approved"
+  | "createdAt"
+  | "updatedAt";
+
+export type CreateUnitRequest = Omit<Unit, HousingLocatorFields>;
 
 export async function createUnit(unit: CreateUnitRequest): Promise<APIResult<Unit>> {
   try {
-    const response = await post("/api/units", unit);
-    const json = (await response.json()) as UnitJSON;
-    return { success: true, data: parseUnit(json) };
+    const response = await post("/units", unit);
+    const json = (await response.json()) as Unit;
+    return { success: true, data: json };
   } catch (error) {
     return handleAPIError(error);
   }
