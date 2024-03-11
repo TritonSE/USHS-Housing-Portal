4a0f206f092e35d64edff41abe080930f88a55a2
PoliteUnicorn <yankaren15@gmail.com>
2024-03-11 16:10:56 -0700

    added authentication for user route

--- backend/src/routes/units.ts
+++ backend/src/routes/units.ts
@@ -20,4 +20,7 @@ router.get("/", UnitController.getUnitsHandler);
 
 router.delete("/:id", requireHousingLocator, UnitController.deleteUnitsHandler);
 
+router.put("/:id", requireHousingLocator, UnitController.getUnitHandler);
+router.put("/:id/approve", requireHousingLocator, UnitController.getUnitHandler);
+
 export default router;
