481340cfa466ac7104f2042c7523cfcbb4b71b3d
Yashwanth Ravipati <yashravipatischool@gmail.com>
2024-05-22 21:39:43 -0700

    Revert "Sending email functionality"
    This reverts commit 4dd63e7c536d1c8a76e3027309747b281b251e61.

--- backend/src/routes/email.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import * as EmailController from "@/controllers/email";
-
-const router = express.Router();
-
-router.post("/", EmailController.sendEmailHandler);
-
-export default router;
