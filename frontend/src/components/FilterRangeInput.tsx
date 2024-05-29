fe3dd29338b30895b690f58e9575f6069c452e44
Ramtin Tajbakhsh <ramtintjb@gmail.com>
2024-05-28 21:58:57 -0700

    Add prefix and suffix to textboxes

--- frontend/src/components/FilterRangeInput.tsx
+++ frontend/src/components/FilterRangeInput.tsx
@@ -10,9 +10,15 @@ const RangeRow = styled.div`
 const RangeInput = styled.input.attrs({
   type: "text",
 })`
-  width: 98px;
+  width: 100%;
   padding: 5px;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
 `;
+
 
 const RangeInputTitle = styled.span`
   color: #b4b4b4;
@@ -34,6 +40,38 @@ const RangeDivider = styled.span`
   border-bottom: 1px solid #cdcaca;
   width: 14px;
   padding-top: 15px;
+`;
+
+const TextBoxContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  padding-left: 5px;
+  padding-right: 5px;
+
+  border-radius: 1.861px;
+  border: 0.465px solid #B4B4B4;
+`;
+
+const TextboxPrefix = styled.span`
+  color: #000;
+  font-family: Montserrat;
+  font-size: 13.596px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 150%; /* 20.394px */
+  letter-spacing: 0.272px;
+`;
+
+const TextboxSuffix = styled.span`
+  color: #000;
+  font-family: Montserrat;
+  font-size: 11px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%; /* 16.5px */
+  letter-spacing: 0.22px;
 `;
 
 export type FilterRangeInputType = "price" | "sqft";
@@ -66,12 +104,20 @@ export const FilterRangeInput = (props: FilterRangeInputProps) => {
     <RangeRow>
       <RangeInputContainer>
         <RangeInputTitle>Min</RangeInputTitle>
-        <RangeInput name="min" value={props.value.min} onChange={changeHandler} />
+        <TextBoxContainer>
+          {props.price === "price" && <TextboxPrefix>$</TextboxPrefix>}  
+          <RangeInput name="min" value={props.value.min} onChange={changeHandler} />
+          {props.price === "sqft" && <TextboxSuffix>sqft</TextboxSuffix>}
+        </TextBoxContainer>
       </RangeInputContainer>
       <RangeDivider />
       <RangeInputContainer>
         <RangeInputTitle>Max</RangeInputTitle>
-        <RangeInput name="max" value={props.value.max} onChange={changeHandler} />
+        <TextBoxContainer>
+          {props.price === "price" && <TextboxPrefix>$</TextboxPrefix>}  
+          <RangeInput name="max" value={props.value.max} onChange={changeHandler} />
+          {props.price === "sqft" && <TextboxSuffix>sqft</TextboxSuffix>}
+        </TextBoxContainer>
       </RangeInputContainer>
     </RangeRow>
   );
